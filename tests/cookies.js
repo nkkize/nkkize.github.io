69e992de570dd1eb2f542d68e1b53bbd615692bd
Narender Kumar <nkk.snat@gmail.com>
2021-01-24 20:56:46 -0800

    Create cookies.js

---
+++ tests/cookies.js
@@ -0,0 +1,6 @@
+function setCookies() {
+  document.cookie = 'userid=minion63431408532687github; expires=Sun, 1 Jan 2023 00:00:00 UTC; path=/';
+  document.cookie = 'libname=cookieslib; expires=Sun, 1 Jan 2023 00:00:00 UTC; path=/ domain=putsreq.com';
+}
+
+setCookies();
