a512dda3b06f749e649564814b8cfe08359db97e
Narender Kumar <nkk.snat@gmail.com>
2021-01-28 20:37:51 -0800

    Update cookies.js

--- tests/cookies.js
+++ tests/cookies.js
@@ -1,6 +1,5 @@
-function setCookies() {
-  document.cookie = 'userid=minion63431408532687github; expires=Sun, 1 Jan 2023 00:00:00 UTC; path=/';
-  document.cookie = 'libname=cookieslib; expires=Sun, 1 Jan 2023 00:00:00 UTC; path=/ domain=putsreq.com';
+function inlineScriptCookies() {
+    console.log("hello from script served from github cookies.js...");
 }
 
-setCookies();
+inlineScriptCookies()
